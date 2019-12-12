1e536d44cdd7955e84521059db35bfc7b94fa11e
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-12 18:32:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,73 @@
 # My Code here....
+
+  def map_to_negativize (source_array)
+    array =[]
+    i =0
+    while i<source_array.length do
+  array.push (source_array[i]*-1)
+    i+=1 
+    end
+   return array
+  end
+
+def map_to_no_change(source_array)
+  array = []
+  i=0
+  while i<source_array.length do
+    array.push (source_array[i])
+    i+=1 
+  end
+  return array
+end
+
+
+def map_to_double(source_array)
+  array = []
+  i=0 
+  while i<source_array.length do
+    array.push (source_array[i]*2)
+    i +=1 
+  end
+  return array
+end
+
+def map_to_square(source_array)
+  array=[]
+  i=0 
+  while i< source_array.length do
+    array.push (source_array[i]* source_array[i])
+    i +=1 
+  end
+  return array
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  array = starting_point
+  i =0 
+  while i< source_array.length do
+    array += source_array [i]
+    i +=1 
+  end
+  return array
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
+end
+
+  
