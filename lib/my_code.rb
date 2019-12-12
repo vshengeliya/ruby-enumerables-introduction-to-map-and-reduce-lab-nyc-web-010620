0a969a41022ebedcf19cf25f293c73b456cb7f37
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-12 16:19:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,13 @@
 # My Code here....
 
 #map_to_negativize = 
-(1, 2, 3, -9).map {|i|i*-1} 
+array = [1, 2, 3, -9]
+array.map {|i|i*-1} 
 
 
 # map_to_no_change(source_array)
 dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+dune.map {|String|String}
 
 # map_to_double(source_array)
 
