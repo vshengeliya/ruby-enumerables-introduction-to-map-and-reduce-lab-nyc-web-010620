e819d3aec7a9ad3cfb1bb2ec9bc11e1d15bb1489
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-12 18:28:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,17 +52,6 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 
-def reduce_to_total(source_array, starting_point=100)
-  array = starting_point
-  i =0 
-  while i< source_array.lenght do
-    new += source_array [i]
-    i +=1 
-  end
-  return array
-end
-
-
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
