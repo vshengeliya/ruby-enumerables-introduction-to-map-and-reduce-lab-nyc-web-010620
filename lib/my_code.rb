c17014c2edc906fe833434599bbd3404e331c799
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-12 18:23:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,19 +64,21 @@ end
 
 
 def reduce_to_all_true(source_array)
-  array =[]
-  i=0 
-  while i<source_array.lenght do
+  i = 0
+  while i < source_array.length do
     return false if !source_array[i]
-
+    i += 1
+  end
+  return true
 end
-# reduce_to_any_true(source_array)
 
-source_array = [ false, nil, nil, nil, true]
-source_array.reduce(:|)
-
-source_array = [ false, nil, nil, nil]
-source_array.reduce(:|)
-
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
+end
 
   
