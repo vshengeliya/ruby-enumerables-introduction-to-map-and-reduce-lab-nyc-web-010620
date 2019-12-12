56751f9e494306aedda27eebb711452c4abea905
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-12 16:25:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,3 +16,7 @@ array.map {|n|n*2}
 # map_to_square(source_array)
 array = [1, 2, 3, -9]
 array.map {|n|n*n}
+
+# reduce_to_total(source_array, starting_point)
+# reduce_to_all_true(source_array)
+# reduce_to_any_true(source_array)
