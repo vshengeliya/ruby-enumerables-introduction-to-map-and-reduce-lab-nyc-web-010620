ac52d5e54812ee2592926db395543230c1eaca2f
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-12 16:14:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
 # My Code here....
 
-def map_to_negativize = 
+#map_to_negativize = 
 (1, 2, 3, -9).map {|i|i*-1} 
-end
 
-map_to_no_change(source_array)
 
-map_to_double(source_array)
+# map_to_no_change(source_array)
 
-map_to_square(source_array)
+# map_to_double(source_array)
+
+# map_to_square(source_array)
